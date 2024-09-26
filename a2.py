9a113abdcaf2d913ac49317953ac2ad250927ed2
mherrera63@cps.edu <mherrera63@cps.edu>
2024-09-26 10:15:30 -0500

    Finised first part of %

--- a2.py
+++ a2.py
@@ -33,7 +33,15 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
-
+        elif pattern[pind] == '%':
+            if pind == len(pattern) - 1:
+                res = " ".join(source[sind :])
+                result.append(res)
+                # pind += 1
+                # sind = len(source)
+                return result 
+            else:
+                return None
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source):
             return None 
